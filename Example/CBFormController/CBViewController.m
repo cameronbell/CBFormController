3e44921b892e942b6c91e212f1097fcaab22e092
Cameron Bell <cameron.bell@me.com>
2015-08-10 13:18:32 -0400

    Initial commit

---
+++ Example/CBFormController/CBViewController.m
@@ -0,0 +1,29 @@
+//
+//  CBViewController.m
+//  CBFormController
+//
+//  Created by Cameron Bell on 08/10/2015.
+//  Copyright (c) 2015 Cameron Bell. All rights reserved.
+//
+
+#import "CBViewController.h"
+
+@interface CBViewController ()
+
+@end
+
+@implementation CBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
