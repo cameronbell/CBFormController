1a24389f0ba3064c1ba04149f38955bf1b3e98b7
Cameron Bell <cameronbell@Camerons-MacBook-Pro.local>
2015-08-11 14:53:06 -0400

    Created category on CBFormItem as part of the CellSet.

---
+++ Pod/Classes/CBFormItem+CBCellSet1.h
@@ -0,0 +1,13 @@
+//
+//  CBFormItem+CBCellSet1.h
+//  Pods
+//
+//  Created by Cameron Bell on 2015-08-11.
+//
+//
+
+#import <CBFormController/CBFormController.h>
+
+@interface CBFormItem (CBCellSet1)
+
+@end
---
+++ Pod/Classes/CBFormItem+CBCellSet1.m
@@ -0,0 +1,15 @@
+//
+//  CBFormItem+CBCellSet1.m
+//  Pods
+//
+//  Created by Cameron Bell on 2015-08-11.
+//
+//
+
+#import "CBFormItem+CBCellSet1.h"
+
+@implementation CBFormItem (CBCellSet1)
+
+-(void)setIcon:(FAIcon *)
+
+@end
