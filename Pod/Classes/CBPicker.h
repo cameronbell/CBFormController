b52650cc4ace00a3aad01e187c38a47fd7b6af9d
Cameron Bell <cameron.bell@me.com>
2015-08-14 10:44:07 -0400

    Casting save/validation block parameters to the right type for CBPicker.

--- Pod/Classes/CBPicker.h
+++ Pod/Classes/CBPicker.h
@@ -11,5 +11,7 @@
 @interface CBPicker : CBFormItem <UIPickerViewDataSource,UIPickerViewDelegate>
 
 @property (nonatomic,retain) NSArray *items;
+@property (nonatomic, copy) void (^save)(NSString *value); //Called to ask the subclass to save the value to the data source
+@property (nonatomic, copy) BOOL (^validation)(NSString *value); //Called to verify that the new value is acceptable to be saved to the data source.
 
 @end
