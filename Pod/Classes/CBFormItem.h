9bedf675b0be3a164ff7d9d976f2d192bd1766a5
Julien Guerinet <julien@guerinet.com>
2015-11-15 18:42:25 -0500

    Merged CBFormItem with CBCell

--- Pod/Classes/CBFormItem.h
+++ Pod/Classes/CBFormItem.h
@@ -8,7 +8,7 @@
 
 @class CBFormController;
 
-@interface CBFormItem : NSObject
+@interface CBFormItem : UITableViewCell
 
 //The FormController instance that this form item belongs to
 @property (nonatomic, weak) CBFormController *formController;
@@ -82,5 +82,18 @@
 
 //Sets an icon in the given color
 - (void)setIcon:(FAIcon)icon withColor:(UIColor *)color;
+
+@property (nonatomic,retain) IBOutlet UILabel *titleLabel; //All cells will have a titleLabel property that they can choose to use or not. This will prevent all subclasses from having to manually set the title if every cell in the CBCellSet uses a titleLabel
+@property (nonatomic,assign) CGFloat height; //This property represents the height of the cell at the dismissed state. This property should not be dynamic under most circumstances. Cells that have different heights based on CBFormItem properties (like engaged) will have properties on their subclasses that represent those heights. The CBFormItem is responsible for telling the formTable what height the cell should be at.
+@property (nonatomic,assign) CGFloat twoLineHeight; //Returns the height when the cell has a double line title.
+@property (nonatomic,retain) IBOutlet UILabel *icon;
+
+@property (nonatomic,assign) CGFloat defaultHeight; //Required method to implement in the subclass
+@property (nonatomic,assign) CGFloat defaultTwoLineHeight;
+
+-(void)setIcon:(UILabel *)icon;
+-(UILabel *)icon;
+
+-(void)configureForFormItem:(CBFormItem *)formItem;
 
 @end
--- Pod/Classes/CBFormItem.m
+++ Pod/Classes/CBFormItem.m
@@ -180,4 +180,104 @@
     [self setIconColor:color];
 }
 
+//Using the address of this char as a key for the associated object created for the custom property
+static char iconKey;
+
+@implementation CBCell
+@synthesize cellSet = _cellSet;
+@synthesize height = _height;
+
+- (void)setIcon:(UILabel *)icon {
+    [self setCustomPropertyWithObject:icon forKey:&iconKey];
+}
+
+- (UILabel *)icon {
+    return (UILabel *)[self getCustomPropertyWithKey:&iconKey];
+}
+
+-(void)configureAddonsForFormItem:(CBText *)formItem {
+    
+    NSNumber *iconInteger = [formItem.addOns objectForKey:@"CBCellSet1_icon"];
+    if (iconInteger) {
+        self.icon.font = [UIFont fontWithName:kFontAwesomeFamilyName size:20];
+        [self.icon setTextColor:[formItem.addOns objectForKey:@"CBCellSet1_icon_color"]];
+        NSString *string = [NSString fontAwesomeIconStringForEnum:[iconInteger integerValue]];
+        [self.icon setText:string];
+        
+    }else {
+        [self.icon setText:@""];
+    }
+}
+
+-(void)configureForFormItem:(CBFormItem *)formItem {
+    
+    //Set the titlelabel's text to the formItem's title - This may not do anything if the cell doesn't use a titleLabel.
+    [self.titleLabel setText:formItem.title];
+    
+    [self setClipsToBounds:YES];
+    
+    //Makes it such that the cell cannot be highlighted by tapping when the form isn't editing, or unless the user has specific that this cell should be able to be interacted with even when the form is not editing
+    if (!([formItem.formController editing] || formItem.enabledWhenNotEditing)) {
+        [self setSelectionStyle:UITableViewCellSelectionStyleNone];
+    }
+    
+    //This function call gives any category on this CBCell an opportunity to customize the cell.
+    if ([self respondsToSelector:@selector(configureAddonsForFormItem:)]) {
+        [self configureAddonsForFormItem:formItem];
+    }
+}
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+//If the subclass doesn't implement this function then return the default for the Cell Set
+-(CGFloat)height {
+    return self.cellSet.defaultHeight;
+}
+
+-(CGFloat)twoLineHeight {
+    return self.cellSet.defaultTwoLineHeight;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+-(void)setCustomPropertyWithObject:(NSObject *)icon forKey:(const void *)key {
+    objc_setAssociatedObject(self, key,
+                             icon, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+}
+
+-(NSObject *)getCustomPropertyWithKey:(const void *)key {
+    return objc_getAssociatedObject(self, key);
+}
+
+-(CGFloat)defaultHeight {
+    return 45;
+}
+
+-(CGFloat)defaultTwoLineHeight {
+    return 70;
+}
+
+-(NSString *)cellClassStringForFormItemClass:(Class)formItemClass {
+    
+    //Remove the CB prefix from the formItemClass string and fail if it doesn't have that prefix.
+    NSString *classString = NSStringFromClass(formItemClass);
+    NSString *noprefixClassString = [NSStringFromClass(formItemClass) copy];
+    if ([classString hasPrefix:@"CB"]){
+        noprefixClassString = [classString substringFromIndex:[@"CB" length]];
+    }else{
+        NSAssert(NO, @"All FormItem classes should have CBPrefix.");
+    }
+    
+    //Return the name of the cell as the concatenation of the name of this CBCellSet subclass and the name of the CBFormItemClass without the CB prefix.
+    return [NSString stringWithFormat:@"%@%@",NSStringFromClass([self class]),noprefixClassString];
+    
+}
+
 @end
