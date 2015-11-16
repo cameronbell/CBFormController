889b46aab048b501e9274f6cdae31c5ba56e0faf
Julien Guerinet <julien@guerinet.com>
2015-11-16 08:43:53 -0500

    Refactored the CBPopupPicker

--- Pod/Classes/CBPopupPicker.h
+++ Pod/Classes/CBPopupPicker.h
@@ -11,12 +11,13 @@
 
 @interface CBPopupPicker : CBFormItem <CBPopupPickerViewDelegate>
 
-@property (nonatomic,retain) IBOutlet UITextField *textField;
-@property (nonatomic,assign) BOOL allowsCustomItems;
-@property (nonatomic,retain) NSMutableArray *items;
-@property (nonatomic, copy) void (^save)(NSString *value); //Called to ask the subclass to save the value to the data source
-@property (nonatomic, copy) BOOL (^validation)(NSString *value); //Called to verify that the new value is acceptable to be saved to the data source.
-
+//The field showing the currently selected item
+@property (nonatomic, retain) IBOutlet UITextField *textField;
+//True if the user can input their own item, false otherwise
+@property (nonatomic, assign) BOOL allowsCustomItems;
+//The array of items to show in the picker
+@property (nonatomic, retain) NSMutableArray *items;
+//Code called when a new item is selected from the picker 
 @property (nonatomic, copy) void (^didSelectItem)(NSString *value);
 
 @end
--- Pod/Classes/CBPopupPicker.m
+++ Pod/Classes/CBPopupPicker.m
@@ -8,21 +8,9 @@
 
 #import "CBPopupPicker.h"
 #import "CBFormController.h"
-#import "CBPopupPickerCell.h"
-
 #import "MZFormSheetController.h"
 
 @implementation CBPopupPicker
-
-@synthesize save;
-@synthesize validation;
-@synthesize allowsCustomItems = _allowsCustomItems;
-@synthesize items = _items;
-@synthesize didSelectItem;
-
--(CBFormItemType)type {
-    return CBFormItemTypePopupPicker;
-}
 
 -(void)configure {
     [super configure];
@@ -32,33 +20,13 @@
     [self.textField setUserInteractionEnabled:NO];
 }
 
-//Ensures that this FormItem's initialValue can only be set to a string
--(void)setInitialValue:(NSObject *)initialValue {
-    if (!initialValue || [initialValue isKindOfClass:[NSString class]]) {
-        _initialValue = initialValue;
-    }else{
-        NSAssert(NO, @"The initialValue of a CBPopupPicker must be a NSString.");
-    }
-}
-
-//Ensures that this FormItem's value can only be set to a string
--(void)setValue:(NSObject *)value {
-    
-    //If the value is nil or not of type NSString then fail, otherwise set the text of the cell's textField to the value
-    if (!value || [value isKindOfClass:[NSString class]]) {
-        [[(CBPopupPickerCell *)self.cell textField] setText:(NSString *)value];
-    }else{
-        NSAssert(NO, @"The value of a CBPopupPicker must be a NSString.");
-    }
-}
-
 -(NSObject *)value {
-    return [(CBPopupPickerCell *)self.cell textField].text;
+    return self.textField.text;
 }
 
 //Ensuring that this never returns nil so that isEdited works properly.
 -(NSObject *)initialValue {
-    return [(NSString *)_initialValue length] ? _initialValue : @"";
+    return [(NSString *)super.initialValue length] ? super.initialValue : @"";
 }
 
 -(BOOL)isEdited {
@@ -67,10 +35,10 @@
 }
 
 -(void)selected {
-    
-    CBPopupPickerView *pickerPopupView = [[CBPopupPickerView alloc]initForFormItem:self withDelegate:self];
-    
-    MZFormSheetController *pickerPopup = [[MZFormSheetController alloc]initWithViewController:pickerPopupView];
+    CBPopupPickerView *pickerPopupView = [[CBPopupPickerView alloc] initForFormItem:self
+                                                                      withDelegate:self];
+    MZFormSheetController *pickerPopup = [[MZFormSheetController alloc]
+                                          initWithViewController:pickerPopupView];
     pickerPopup.shouldDismissOnBackgroundViewTap = YES;
     pickerPopup.transitionStyle = MZFormSheetTransitionStyleSlideFromBottom;
     pickerPopup.cornerRadius = 8.0;
@@ -82,22 +50,19 @@
     pickerPopup.presentedFormSheetSize = CGSizeMake(screenSize.size.width - 40, 350);
     
     pickerPopup.willPresentCompletionHandler = ^(UIViewController *presentedFSViewController){
-        presentedFSViewController.view.autoresizingMask = presentedFSViewController.view.autoresizingMask | UIViewAutoresizingFlexibleWidth;
+        presentedFSViewController.view.autoresizingMask =
+            presentedFSViewController.view.autoresizingMask | UIViewAutoresizingFlexibleWidth;
     };
     
-    [pickerPopup presentAnimated:YES completionHandler:^(UIViewController *presentedFSViewController) {
-        
-    }];
+    [pickerPopup presentAnimated:YES
+               completionHandler:^(UIViewController *presentedFSViewController) {}];
     
     [super selected];
-
 }
 
 
--(void)popupPickerForFormItem:(CBFormItem*)formItem didSelectItem:(NSString *)item {
-    CBPopupPickerCell *cell =  (CBPopupPickerCell *)self.cell;
-    [cell.textField setText:item];
-    
+-(void)popupPickerForFormItem:(CBFormItem *)formItem didSelectItem:(NSString *)item {
+    [self.textField setText:item];
     [self.formController mz_dismissFormSheetControllerAnimated:YES completionHandler:nil];
     
     if ([self isEdited]) {
@@ -107,11 +72,13 @@
     if (self.didSelectItem) {
         self.didSelectItem(item);
     }
-    
-    
-    
 }
 
-
+//Checks that the given value is a String
+- (void) validateValue:(NSObject *)value {
+    if (![value isKindOfClass:[NSString class]]) {
+        NSAssert(NO, @"The value of a CBPopupPicker must be a NSString.");
+    }
+}
 
 @end
