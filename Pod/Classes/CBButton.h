6d60e3d00214f0f940eecedf896974f058165b12
Julien Guerinet <julien@guerinet.com>
2015-11-15 19:39:12 -0500

    Refactored the CBButton

--- Pod/Classes/CBButton.h
+++ Pod/Classes/CBButton.h
@@ -8,20 +8,11 @@
 
 #import "CBFormItem.h"
 
-//All CBFormItems have a type which corresponds with an enum value
-typedef NS_ENUM(NSInteger, CBButtonType) {
-    CBButtonTypeNone,
-    CBButtonTypeDelete,
-    CBButtonTypeAdd,
-    CBButtonTypeSave,
-    CBButtonTypeContinue
-};
-
-
 @interface CBButton : CBFormItem
 
-@property (nonatomic,assign) CBButtonType buttonType;
-@property (nonatomic,assign) NSTextAlignment titleAlign; //Controls the alignment of the title in the button cell
-@property (nonatomic,retain) UIColor *titleColor;
+//Title alignment
+@property (nonatomic, assign) NSTextAlignment titleAlignment;
+//Title color
+@property (nonatomic, retain) UIColor *titleColor;
 
 @end
--- Pod/Classes/CBButton.m
+++ Pod/Classes/CBButton.m
@@ -10,9 +10,7 @@
 #import "CBFormController.h"
 
 @implementation CBButton
-@synthesize titleAlign = _titleAlign;
-@synthesize titleColor = _titleColor;
-@synthesize buttonType = _buttonType;
+
 @synthesize save;
 @synthesize validation;
 
@@ -26,51 +24,11 @@
     return self;
 }
 
--(CBFormItemType)type {
-    return CBFormItemTypeButton;
-}
-
--(void)configureCell:(CBCell *)cell {
+-(void)configure {
     [super configure];
+    
     [self.titleLabel setTextAlignment:self.titleAlign];
     [self.titleLabel setTextColor:self.titleColor];
-}
-
-//If the buttonType property is set, it takes precedence over the titleAlign property
--(NSTextAlignment)titleAlign {
-    //Defaults to CBButtonTypeNone == 0 so this evaluates to NO
-    if (_buttonType) {
-        switch (_buttonType) {
-            case CBButtonTypeDelete:
-            case CBButtonTypeSave:
-                return NSTextAlignmentCenter;
-            case CBButtonTypeContinue:
-                return NSTextAlignmentRight;
-            default:
-                return NSTextAlignmentLeft;
-                break;
-        }
-    }else{
-        return _titleAlign;
-    }
-}
-
-// The title ivar takes precedence but if no title is specified then titles are provided based on the button type if applicable.
--(NSString *)title {
-    if (_title) {
-        return _title;
-    }else{
-        switch (self.buttonType) {
-            case CBButtonTypeDelete:
-                return @"Delete";
-            case CBButtonTypeSave:
-                return @"Save";
-                break;
-            default:
-                return @"";
-                break;
-        }
-    }
 }
 
 -(UIColor *)titleColor {
