e933676583b6f2e0e03c5cdce0bf5ac23ec08d90
Julien Guerinet <julien@guerinet.com>
2017-01-17 11:10:25 -0500

    Moved the selected call to before the default value is set

--- CBFormController/Classes/CBDate.m
+++ CBFormController/Classes/CBDate.m
@@ -32,17 +32,17 @@
 
 //Ensures that this FormItem's value can only be set to a date
 -(void)setValue:(NSObject *)value {
-    
+
     // If the value is a nsdate or is nil
     if ([value isKindOfClass:[NSDate class]] || !value) {
         _value = value;
-        
+
         CBDateCell *dateCell = (CBDateCell *)self.cell;
-        
+
         NSString *dateString = [self.dateFormatter stringFromDate:_value];
         UITextField *dateField = [dateCell dateField];
         [dateField setText:dateString];
-        
+
         // If value is not nil then set the date of the picker
         if (_value) {
             [dateCell.datePicker setDate:_value];
@@ -50,8 +50,8 @@
         }else{
             [dateCell.clearButton setEnabled:NO];
         }
-        
-        // Always call valueChanged unless 
+
+        // Always call valueChanged unless
         if (self.formController.editMode == CBFormEditModeEdit) {
             if([self isEdited]) [self valueChanged];
         } else {
@@ -81,13 +81,13 @@
 }
 
 -(BOOL)isEdited {
-    
+
     //If both value and initialValue are nil then the formItem has not been edited
     if (![self value] && ![self initialValue]) return NO;
-    
+
     //If one is nil and the other is not then one of them has changed
     if ((!self.initialValue)^(!self.value)) return YES;
-    
+
     return ![CBDate date:(NSDate *)self.initialValue isSameDayAsDate:(NSDate *)self.value];
 }
 
@@ -95,13 +95,13 @@
     // Both dates must be defined, or they're not the same
     //assertnotnil(date1);
     //assertnotnil(date2);
-    
+
     if (date1 == nil || date2 == nil) {
         return NO;
     }
-    
+
     NSCalendar *calendar = [[NSCalendar alloc]initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-    
+
     NSDateComponents *day = [calendar components:NSCalendarUnitEra|NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:date1];
     NSDateComponents *day2 = [calendar components:NSCalendarUnitEra|NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:date2];
     return ([day2 day] == [day day] &&
@@ -114,14 +114,14 @@
 //Updating the height of the cell is the only change required for engaging an dismissing CBDate items
 -(void)engage {
     [super engage];
-    
+
+    [self selected];
+
     // If no value nor initialValue is provided then load the current date
     if (!_value && !_initialValue) {
         [self setValue:[NSDate date]];
     }
-    
-    [self selected];
-    
+
     [self.formController updates];
 }
 -(void)dismiss {
@@ -130,10 +130,10 @@
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField {
-    
+
     //Let the formController decide the actual return value of this function as it manages the selection of formitems in response to the return key.
     return [self.formController textFieldShouldReturnForFormItem:self];
-    
+
 }
 
 //If the CBDate is engaged then return the CBDateCell's engagedHeight, otherwise just fall back to the default.
